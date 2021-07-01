3f540fc0b0dd970c81ea5662d1756a4b52e6e598
Aditya <adityalalwani05@gmail.com>
2021-07-01 13:34:46 +0530

    Majority Element in array

--- LeetCode/MajorityElement.cpp
+++ LeetCode/MajorityElement.cpp
@@ -1,4 +1,43 @@
-//Majority element - First Try
+//Second Try
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        
+        //find the candidate
+        int maj_index = 0;
+        int count = 1;
+        for(int i = 1 ; i < n ; i++)
+        {
+            if(nums[i] == nums[maj_index])
+                count++;
+            else
+                count--;
+            
+            if(count == 0)
+            {
+                maj_index = i;
+                count = 1;
+            }
+        }
+        
+        count = 0;
+        //check for majority elemnt
+        for(auto &i: nums)
+        {
+            if(i == nums[maj_index])
+                count++;
+        }
+        if(count > n/2)
+            return nums[maj_index];
+        else
+            return -1;
+    }
+};
+
+
+/*
+//First Try
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
@@ -21,4 +60,4 @@ public:
         
         return -1;
     }
-};
+};*/
